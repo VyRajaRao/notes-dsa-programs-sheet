4548057d28d455189a7e8bd5fc55cc0aa01b79bd
VyRajaRao49 <vyrajarao49@gmail.com>
2025-11-18 00:37:35 +0530

    Graph code

---
+++ graph.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+    int V;
+    list<pair<int, int>>* l;
+public:
+    Graph(int V) {
+        this->V = V;
+        l = new list<pair<int, int>> [V];
+    }
+
+    void addEdges(int u, int v, int wt) {
+        l[u].push_back(make_pair(v, wt)); 
+    }
+
+    void print() {
+        for(int u=0; u<V; u++) {
+            list<pair<int, int>> neighbors = l[u];
+            cout << u << ": " << " ";
+            for(pair<int, int> v : neighbors) {
+                cout << "(" << v.first << "," << v.second <<") ";
+            }
+            cout << endl;
+        }
+    }
+};
+int main() {
+    Graph graph(5);
+
+    graph.addEdges(0,1,5);
+    graph.addEdges(1,0,5);
+    graph.addEdges(1,2,1);
+    graph.addEdges(1,3,3);
+    graph.addEdges(2,1,1);
+    graph.addEdges(2,3,1);
+    graph.addEdges(2,4,2);
+    graph.addEdges(3,2,1);
+    graph.addEdges(3,1,3);
+    graph.addEdges(4,2,2);
+
+    graph.print();
+}
